e8bb81e4c76f383775b9f362f9712ed086074ead
bad4iz <bad4iz@gmail.com>
2017-09-06 10:30:54 +0400

    init pug linter es linter material-design-lite

---
+++ source/pages/index/index.js
@@ -0,0 +1,42 @@
+import './index.scss';
+import 'normalize.css';
+
+import 'm'
+
+import knockout from 'knockout';
+
+
+class Course {
+  constructor(title, price) {
+    this.title = knockout.observable(title);
+    this.price = knockout.observable(price);
+  }
+}
+
+class Customer {
+  constructor() {
+    this.name = knockout.observable('Guest');
+    this.location = knockout.observable('Moscow');
+    this.info = knockout.computed(() => `Привет ${this.name()} из ${this.location()}`);
+    this.coursesList = knockout.observableArray([
+      new Course('hhh', 111),
+      new Course('aaa', 222),
+      new Course('bbbb', 333),
+      new Course('ccc', 444),
+    ]);
+  }
+
+  getName() {
+    const name = prompt('как вас зовут', this.name());
+    this.name(name);
+  }
+
+  getLocation() {
+    const location = prompt('location', this.location());
+    this.location(location);
+  }
+}
+
+
+knockout.applyBindings(new Customer());
+
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+
+const sass = require('./webpack/sass');
+
+const css = require('./webpack/css');
+
+const extractCSS = require('./webpack/css.extract');
+
+const uglifyJS = require('./webpack/js.uglify');
+
+const images = require('./webpack/images');
+
+const babel = require('./webpack/babel');
+
+const PATHS = {
+  source: path.join(__dirname, 'source'),
+  build: path.join(__dirname, 'build')
+};
+
+process.traceDeprecation = true;
+
+const common = merge([
+  {
+    entry: {
+      'index': PATHS.source + '/pages/index/index.js',
+    },
+    output: {
+      library: "bad4iz",
+      path: PATHS.build,
+      filename: 'js/[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        chunks: ['index', 'common'],
+        template: PATHS.source + '/pages/index/index.pug'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'common'
+      }),
+
+    ],
+  },
+  pug(),
+  babel(),
+  images(),
+]);
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge([
+      common,
+      extractCSS(),
+
+      uglifyJS(),
+    ]);
+  }
+  if (env === 'development') {
+    return merge([
+      common,
+      {
+        devtool: 'source-map'
+      },
+
+      devserver(),
+      sass(),
+      css()
+    ])
+  }
+}
